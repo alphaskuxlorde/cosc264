0c6fdd2e802161b7d6a8f8b884c3f886c31784ee
Chris Wong <lambda.fairy@gmail.com>
2015-08-26 17:16:43 +1200

    Fix typo in integration test

--- integration-test.sh
+++ integration-test.sh
@@ -6,7 +6,7 @@ output='README.md.out'
 for p_rate in 0 0.2 0.5 0.9
 do
     echo '### Testing with P =' $p_rate
-    rm -f "$OUTPUT"
+    rm -f "$output"
     ./run-everything.sh "$input" "$output" $p_rate
     if diff -U3 "$input" "$output"
     then
